a4229135854da2e760f433fc49aea4b82e420cce
Stepa <styopeek@gmail.com>
2025-06-05 13:48:58 +0500

    Update test_model.py
    edit unit tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,9 +1,9 @@
 import os
 import sys
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from model import predict_toxicity
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 def test_predict_returns_bool():
